dd1e7ce1ffed5767a72a319b42535ad4b641ab75
Artun Çolak <artuncolak97@gmail.com>
2019-08-15 00:33:08 +0300

    Gpa calculator model fixed

--- src/Validator.java
+++ src/Validator.java
@@ -1,7 +1,7 @@
 import Exceptions.MaxRowNumberReachedException;
 
 public class Validator {
-    private static final int MAX_ROW = 15;
+    private static final int MAX_ROW = 25;
 
     public static void validateRowNumber(int row) throws MaxRowNumberReachedException {
         if (row >= MAX_ROW)
