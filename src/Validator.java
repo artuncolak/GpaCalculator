321ecbffaeb011923fb7fc06c42fcd3057054036
Artun Çolak <artuncolak97@gmail.com>
2019-09-07 01:17:11 +0300

    Validator Changed

--- src/Validator.java
+++ src/Validator.java
@@ -3,7 +3,7 @@ import Exceptions.MaxRowNumberReachedException;
 public class Validator {
     private static final int MAX_ROW = 25;
 
-    public static void validateRowNumber(int row) throws MaxRowNumberReachedException {
+    static void validateRowNumber(int row) throws MaxRowNumberReachedException {
         if (row >= MAX_ROW)
             throw new MaxRowNumberReachedException();
     }
